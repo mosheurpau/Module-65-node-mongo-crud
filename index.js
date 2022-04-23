90fd174b4733968b380035620f02910875608365
mosheurpau <mosheur.pau@gamil.com>
2022-04-23 15:39:55 +0600

    Create mongodb atlas account and Connect to database

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+// user: dbuser1
+// password: 78FQbYrnLvsaAOWh
+
+const uri =
+  "mongodb+srv://dbuser1:78FQbYrnLvsaAOWh@cluster0.zrcp4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("db connected");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Running My Node CRUD Server");
